48c66eeba9389361e140951f424c1a5a8e587b6b
Matt Slay <mattslay@jordanmachine.com>
2017-03-12 19:21:34 -0500

    Added some comments to app.component.ts, removed unused methods from data.service.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
-//import { DataComponent } from './data.component';
 import { DataService } from './data.service';
 import { Observable }       from 'rxjs/Observable';
 
@@ -16,31 +15,31 @@ export class AppComponent implements OnInit {
    vehicles: Vehicle[];
 
    constructor(private dataService: DataService) {
-      //this.vehicle = <Vehicle>{name: 'Corvette', manufacturer: 'Chevrolet'};
-      //this.vehicle = <Vehicle>{};
       this.vehicle = new Vehicle();
       this.vehicles = [];
    }
 
+   // Angular automatically calls this method when this class is references in the UI.
+   // In this method, we will get the initial list of Vehicles and the app.html page
+   // renders them using *ngFor
    ngOnInit() {
-          console.log('getting data...');
-
-         this.dataService.getVehicleById(30).subscribe(z => {
-           //this.vehicle = z;
-           console.log(JSON.stringify(this.vehicle));
-         });
+         console.log('getting data...');
 
          this.dataService.getVehicles().subscribe(x => this.vehicles = x);
-         // this.dataService.getAllPages('').subscribe(x => this.vehicles = x);
-         //this.vehicles = this.dataService.getAllPages('');
       }
 
+    // This method is called from the UI when the user clicks on a Vehicle button in the list.
+    // After this method makes the API call, once the data is returned, we set this.Vehicle automatically
+    // the object data we receive back, and the UI will automatically update the Vehicle detail div
+    // with the new data.
     selectVehicle(url: string) {
          this.dataService.getVehicleByUrl(url).subscribe(z => this.vehicle = z);
     }
 
  }
 
+// These are only a few of the fields on the Vehicle object, but the UI can render and of them just fine.
+// To get the full of TypeScript intellisense, we really should list all the property names from the API here.
 export class Vehicle {
     name: string;
     manufacturer: string;
--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -31,88 +31,11 @@ export class DataService {
 
 
     getVehicles(): Observable<Vehicle[]> {
+            // Note: this API call only gets the first page of data. We could extend
+            // this method to make additional calls to get the rest of the pages.
+            // There are about 6 pages of data in total.
             return this._http.get(this._vehiclesUrl, this._params).map(res => res.json().results);
-                // {params: {format: 'json', page: page}}
     }
-
-    _getPage(page: number) {
-
-    }
-    getVehicles1(): Observable<Vehicle[]> {
-        // see: http://codepen.io/lancegliser/pen/GjXpNE?editors=0010
-
-            let responseData: any;
-            let vehicles: Vehicle[];
-
-            return this._http.get(this._vehiclesUrl).map(res => {
-                responseData = res.json();
-                vehicles = responseData.results;
-                JSON.stringify(vehicles);
-                if (responseData.next) {
-                    this._http.get(responseData.next).map(res2 => {
-                        vehicles.concat(res2.json().results);
-                    return vehicles;
-                    });
-                } else {
-                    return vehicles;
-                }
-            });
-
-            //return this._http.get(url).map(res => res.json());
-            //todo: the above call only gets one page of data. there are multiple pages,
-            // so will have to call multuple times to get it all.
-    }
-
-    getAllPages(url: string) {
-
-           if (url == '') { url = this._vehiclesUrl;}
-
-           //let result = { next: string, vehicles: Vehicle[] }
-
-           return this.getPage(url)
-           .map(resp => {
-               let data = resp.json()
-                if (data.next)
-                {
-                    return this.getAllPages(data.next)
-                    .map(resultsToJoin => [data.results, resultsToJoin.json().results]);
-                } else {
-                    return data.results;
-                }
-
-           });
-    }   
-
-    getPage(url: string) {
-
-           let vehicles: Vehicle[] = [];
-
-            //return this._http.get(url, this._params).map(res => res.json().results);
-            return this._http.get(url, this._params);
-           
-
-    }
-    
-        getPage2(url: string) {
-
-           let vehicles: Vehicle[] = [];
-
-            if (url == '') { url = this._vehiclesUrl;}
-
-            return this._http.get(url).map(res => {
-                let responseData = res.json();
-                vehicles = responseData.results;
-                if (responseData.next) {
-                    this.getPage(responseData.next).map(x => {
-                        vehicles = vehicles.concat(x.json().results);
-                        return vehicles;
-                    });
-                } else {
-                    return vehicles;
-                }
-            })
-    }
-
 
 
     private handleError(error: Response) {
